94cb3b400007da396b68bce65d0fed2a5fd2e4aa
vencax <vencax@centrum.cz>
2018-06-06 07:58:32 +0200

    process.env.DOTENV_CONFIG

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({path: process.env.DOTENV_CONFIG || '.env'})
 const express = require('express')
 const cors = require('cors')
 const port = process.env.PORT || 3000
